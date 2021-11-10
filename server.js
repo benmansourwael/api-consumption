a33211d34f445ba598d63046d9567916d99aaf77
Wael Ben Mansour <Benmansourwael@live.fr>
2021-11-10 10:25:50 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/angular-project'));
+app.use(express.static(__dirname + '/dist/angularProject'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/angular-project/index.html'));});
+'/dist/angularProject/index.html'));});
 app.listen(process.env.PORT || 8080);
